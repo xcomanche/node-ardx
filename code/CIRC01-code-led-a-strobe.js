8ef64c9763080962939b91bfdaffd02714e71ba0
comanche <xcomanche@gmail.com>
2014-12-07 01:43:30 +0200

    added zmq server connectivity

--- code/CIRC01-code-led-a-strobe.js
+++ code/CIRC01-code-led-a-strobe.js
@@ -5,7 +5,7 @@ myBoard = new j5.Board();
 
 myBoard.on("ready", function() {
 
-  myLed = new myBoard.exports['Led'](13);
+  myLed = new j5.Led(13);
 
   myLed.on();
 
@@ -16,5 +16,6 @@ myBoard.on("ready", function() {
   this.repl.inject({
   	led: myLed
   });
+
   console.log("You can interact with the RGB LED via the variable 'led' e.g. led.on();\n Hit control-D to exit.\n >> ");
 });
---
+++ code/rshuv10-code-zmq-server.js
@@ -0,0 +1,94 @@
+var j5      = require("johnny-five");
+var zmq     = require('zmq'),
+    server  = zmq.socket('push'),
+    client  = zmq.socket('pull');
+var myBoard,
+    devices = [];
+
+server.bindSync('tcp://127.0.0.1:3000');
+console.log('Radiostation connected to port 3000');
+
+client.connect('tcp://127.0.0.1:3001');
+console.log('Listener connected to port 3001');
+
+myBoard = new j5.Board();
+
+myBoard.on("ready", function() {
+
+    client.on('message', function(resp){
+        var responseObj = JSON.parse(resp);
+
+        if (responseObj['init']) {
+            printObjRecv(responseObj);
+            if (responseObj['initObject'] in j5) {
+                devices[responseObj['id']] = j5[responseObj['initObject']](responseObj['initParams']);
+                responseObj['status'] = 3;
+                response(responseObj);
+            } else {
+                console.log('ERROR: Such Object DOES NOT EXISTS!');
+                console.log('-----------------------');
+                responseObj['status'] = 5;
+                responseObj['error'] = 'Object does not exist';
+                response(responseObj);
+            }
+
+        }
+
+        if (responseObj['command']) {
+            responseObj['params'] = (responseObj['params']) ? responseObj['params'] : '';
+            printDevRecv(responseObj);
+
+            if (devices[responseObj['id']]) {
+
+                if (devices[responseObj['id']][responseObj['command']]) {
+                    devices[responseObj['id']][responseObj['command']].apply(devices[responseObj['id']],
+                                                                                 parseDevParams(responseObj['params']));
+                    responseObj['status'] = 3;
+                    response(responseObj);
+                } else {
+                    console.log('ERROR: DEVICE: ' + responseObj['id']
+                    + ' DOES NOT HAVE COMMAND: '  + responseObj['command']);
+                    console.log('-----------------------');
+                    responseObj['status'] = 5;
+                    responseObj['error'] = 'Command does not exist';
+                    response(responseObj);
+                }
+            } else {
+                console.log('ERROR: Device: ' + responseObj['id'] + ' DOES NOT EXISTS!');
+                console.log('-----------------------');
+                responseObj['status'] = 5;
+                responseObj['error'] = 'Device does not exist';
+                response(responseObj);
+            }
+        }
+    });
+});
+
+function printObjRecv(responseObj) {
+    console.log('New Device Initialized!');
+    console.log(' - ID: ' + responseObj['id']);
+    console.log(' - Object: ' + responseObj['initObject']);
+    console.log(' - Params: ' + responseObj['initParams']);
+    console.log('-----------------------');
+}
+
+function printDevRecv(responseObj) {
+    console.log('New Command Received!');
+    console.log(' - ID: ' + responseObj['id']);
+    console.log(' - Command: ' + responseObj['command']);
+    console.log(' - Params: ' + responseObj['params']);
+    console.log('-----------------------');
+}
+
+function parseDevParams(paramsString) {
+    // Split params by '|' delimiter
+    return (paramsString.indexOf('|') === -1) ? [paramsString] : paramsString.split('|');
+}
+
+function response(response) {
+    server.send(JSON.stringify(response))
+}
+
+function responseCallBack(ev) {
+    server.send(JSON.stringify(ev));
+}
